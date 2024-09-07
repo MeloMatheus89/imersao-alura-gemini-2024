a86e327076d48ec97bf427ccd51160d56e1068af
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2024-09-06 21:28:07 -0300

    Add files via upload
    primeira versão

---
+++ app.js
@@ -0,0 +1,47 @@
+//função que pesquisa no objeto dados e exibe as informações
+function pesquisar() {
+    // Seleciona a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("busca").value;
+    
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    console.log(campoPesquisa);
+    let  nome ="";
+    let aplicacao = "";
+    let tags = "";
+     
+    if(campoPesquisa == ""){
+      section.innerHTML = "<p>Não foi encontrado nenhum produto ou tecnologia. Você precisa digitar algo neste campo.</p>"
+      return;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+  
+    // Itera sobre cada item do array de dados
+    for (let item of dados) {
+      nome = item.nome.toLowerCase();
+      aplicacao = item.aplicacao.toLowerCase();
+      tags = item.tags.toLowerCase();
+      //se titulo includes campoPesquisa
+        //então faça...
+        if (tags.includes(campoPesquisa) || nome.includes(campoPesquisa) || aplicacao.includes(campoPesquisa)){
+          resultados += `
+            <div class="item-resultado">
+              <h2>${item.nome}</h2>
+              <p class="descricao-meta">${item.aplicacao}</p>
+              <p class="descricao-meta">${item.exemplo}</p>
+              <a href="${item.link}" target="_blank">Mais informações</a>
+            </div>
+          `;
+        } else {
+          section.innerHTML = "<p>Não foi encontrado nenhum produto ou tecnologia</p>"
+          return;
+        }
+      console.log(item.nome.includes(campoPesquisa));
+      // Constrói o HTML para cada resultado, formatando as informações do item
+    }
+  
+    // Atribui o HTML gerado para a seção de resultados, substituindo o conteúdo anterior
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,47 @@
+let dados = [
+        {
+        nome: 'Switch Camada 2',
+        aplicacao: 'Um switch não gerenciável é um dispositivo de rede plug-and-play, projetado para conectar múltiplos dispositivos em uma rede local (LAN) de forma simples e automática. Sem a necessidade de configurações complexas, ele funciona como um hub inteligente, encaminhando dados entre os dispositivos conectados com base em seus endereços MAC. Ideal para pequenas redes domésticas ou pequenas empresas que buscam uma solução fácil e rápida, os switches não gerenciáveis oferecem conectividade básica e confiável, sem a necessidade de um administrador de rede.',
+        link: 'https://pt.wikipedia.org/wiki/Comutador_(redes)',
+        exemplo: 'S2328G-PA',
+        tags: "switch comutador camada gerenciável gerenciamento"
+        },
+        {
+          nome: 'Access Points',
+          aplicacao: 'Um access point fornece conectividade Wi-Fi, permitindo que dispositivos sem fio se conectem a uma rede cabeada. Ideal para criar redes wireless em empresas, escritórios e locais públicos.',
+          link: 'https://pt.wikipedia.org/wiki/Ponto_de_acesso',
+          exemplo: 'AP 1800 AX',
+          tags: "wifi wi-fi 4 5 6 ap "
+        },
+        {
+          nome: 'Switch Camada 3',
+          aplicacao: 'Um switch de camada 3 realiza roteamento de pacotes com base no endereço IP, além das funções de um switch de camada 2. Ideal para redes maiores e complexas.',
+          link: 'https://pt.wikipedia.org/wiki/Roteador',
+          exemplo: 'S3328G-PB',
+          tags: "gerenciável gerenciavel rota camada 3 bgp ospf is-is gerenciamento"
+
+        },
+        {
+          nome: 'Switch Não Gerenciáveis',
+          aplicacao: 'Um switch não gerenciável é um dispositivo plug-and-play que conecta dispositivos em uma rede local sem a necessidade de configurações complexas. Ideal para pequenas redes domésticas.',
+          link: 'https://pt.wikipedia.org/wiki/Comutador_(redes)',
+          exemplo: 'S1124G',
+          tags: "comutador switch plug and play "
+        },
+        {
+          nome: 'Roteador Residencial',
+          aplicacao: 'Um roteador residencial conecta múltiplos dispositivos a uma rede e à internet, funcionando como um gateway. Geralmente oferece funcionalidades como Wi-Fi, NAT e firewall.',
+          link: 'https://pt.wikipedia.org/wiki/Roteador',
+          exemplo: 'Fiberhome SR1041E',
+          tags: "wifi 4 5 6 protocolo ac ax "
+        },
+        {
+          nome: 'Controladoras Wi-Fi',
+          aplicacao: 'Um controlador Wi-Fi gerencia e configura múltiplos access points em uma rede wireless, oferecendo recursos como balanceamento de carga e roaming sem costura.',
+          link: 'https://en.wikipedia.org/wiki/Wireless_LAN_controller',
+          exemplo: 'Intelbras WC 3020',
+          tags: "controller empresarial densidade ap gerenciamento"
+      },
+
+
+];
